6fe61ca7ba4f38f40c4ad742965d24f083b565db
User1 <user1@Skycliffitdomain.com>
2019-08-20 17:42:14 +0530

    implemented interceptors

---
+++ src/app/interceptordemo.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
+export class Interceptordemo implements HttpInterceptor {
+
+intercept(req:HttpRequest<any>,next:HttpHandler)
+{
+  //req.headers.set('Content-Tyep','application/json');
+  console.log('inside intercept');
+  const httpreq=req.clone({
+    headers:req.headers.set('Content-Type','application/json')
+  });
+  return next.handle(httpreq);
+}
+
+}
