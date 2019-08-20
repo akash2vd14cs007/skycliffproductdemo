677da160ee485b5d81a0c1a3c6e70cc73395b553
User1 <user1@Skycliffitdomain.com>
2019-08-20 14:34:52 +0530

    initialcommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ProductdisplayComponent } from './productdisplay/productdisplay.component';
+import { MenuComponent } from './menu/menu.component';
+import { routing } from './app.routing';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductaddComponent } from './productdisplay/productadd/productadd.component';
+import { UserdisplayComponent } from './userdisplay/userdisplay.component';
+import { SignupComponent } from './userdisplay/signup/signup.component';
+import { EdituserComponent } from './userdisplay/edituser/edituser.component';
+import { EditproductComponent } from './productdisplay/editproduct/editproduct.component';
+import { TaskdisplayComponent } from './taskdisplay/taskdisplay.component';
+import { AddtaskComponent } from './taskdisplay/addtask/addtask.component';
+import { EdittaskComponent } from './taskdisplay/edittask/edittask.component';
+import { Sidebar2Component } from './sidebar2/sidebar2.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SignupReactiveDemoComponent } from './userdisplay/signup-reactive-demo/signup-reactive-demo.component';
+import { EdituserreactiveComponent } from './userdisplay/edituserreactive/edituserreactive.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductdisplayComponent,
+    MenuComponent,
+    PagenotfoundComponent,
+    SidebarComponent,
+    ProductaddComponent,
+    UserdisplayComponent,
+    SignupComponent,
+    EdituserComponent,
+    EditproductComponent,
+    TaskdisplayComponent,
+    AddtaskComponent,
+    EdittaskComponent,
+    Sidebar2Component,
+    CustomerComponent,
+    SignupReactiveDemoComponent,
+    EdituserreactiveComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,37 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ProductdisplayComponent } from './productdisplay/productdisplay.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProductaddComponent } from './productdisplay/productadd/productadd.component';
+import { UserdisplayComponent } from './userdisplay/userdisplay.component';
+import { SignupComponent } from './userdisplay/signup/signup.component';
+import { EdituserComponent } from './userdisplay/edituser/edituser.component';
+import { EditproductComponent } from './productdisplay/editproduct/editproduct.component';
+import { EdittaskComponent } from './taskdisplay/edittask/edittask.component';
+import { AddtaskComponent } from './taskdisplay/addtask/addtask.component';
+import { TaskdisplayComponent } from './taskdisplay/taskdisplay.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SignupReactiveDemoComponent } from './userdisplay/signup-reactive-demo/signup-reactive-demo.component';
+import { EdituserreactiveComponent } from './userdisplay/edituserreactive/edituserreactive.component';
+import {LoginComponent} from './login/login.component';
+import { UserGuardService } from './userguard.service';
+
+const arr: Routes = [
+  {path:'',component:ProductdisplayComponent},
+  {path:'addproduct',canActivate:[UserGuardService],component:ProductaddComponent},
+  {path:'editproduct/:pro_id',component:EditproductComponent},
+  {path:'users',component:UserdisplayComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'edituser/:user_email',component:EdituserComponent},
+   {path:'addtask',component:AddtaskComponent},
+   {path:'task',component:TaskdisplayComponent},
+  {path:'edittask/:Id',component:EdittaskComponent},
+  {path:'pagenotfound',component:PagenotfoundComponent},
+  {path:'customer', component:CustomerComponent},
+  {path:'signup2', component:SignupReactiveDemoComponent},
+  {path:'editreactive/:user_email',component:EdituserreactiveComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**',redirectTo:'/pagenotfound'},
+
+];
+
+export const routing = RouterModule.forRoot(arr);
